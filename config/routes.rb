26b75ed21927708db529283afc733aed43ff92b3
Jviveiros <jviveiros@github.com>
2017-01-25 15:29:55 -0800

    Fixed route bug

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
   root "users#index"
+
   resources :users, only: [:index, :new, :show, :create, :edit]
-  resources :sessions, only: [:new, :create, :delete]
+
+  resources :sessions, only: [:new, :create, :destroy]
+
   get "/slots/:year/:month", to: "slots#month_index"
+
   get "/slots/:year/:month/:day", to: "slots#day_index"
+
   resources :slots, except: [:edit, :update, :index]
 
   resources :feedbacks
