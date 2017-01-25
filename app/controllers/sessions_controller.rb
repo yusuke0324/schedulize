25d54e964b5031735bea065cad8d17f0ee9cf6d4
Jviveiros <jviveiros@github.com>
2017-01-25 12:20:41 -0800

    New user registration  and user auth in place

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    if @user = login(params[:email], params[:password])
+       redirect_back_or_to(root_path, notice: 'Login successful')
+    else
+      flash.now[:alert] = 'Login failed'
+      render action: 'new'
+    end
+  end
+
+  def destroy
+    logout
+    redirect_to(root_path, notice: 'Logged out!')
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+
+
+
+
+
+
+
+
+
+
+resources :users, only: [:index, :new, :show, :create, :edit]
+resources :sessions, only: [:new, :create, :delete]
+
+root "users#index"
 end
