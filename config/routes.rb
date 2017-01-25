05c901e075ecac843df36c910831b03778f78f88
Jviveiros <jviveiros@github.com>
2017-01-25 15:06:17 -0800

    Added Login, Logout functionality

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
 
 
 resources :users, only: [:index, :new, :show, :create, :edit]
-resources :sessions, only: [:new, :create, :delete]
+resources :sessions, only: [:new, :create, :destroy]
 
 root "users#index"
 end
