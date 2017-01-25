af1d711f51b8c9cf48f7acc225282b242fbe23f4
Jviveiros <jviveiros@github.com>
2017-01-25 15:26:57 -0800

    Fixed merge error on routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,31 +1,10 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-
-
-
-
-
-
-
-
-
-
-
-
-resources :users, only: [:index, :new, :show, :create, :edit]
-resources :sessions, only: [:new, :create, :destroy]
-
-root "users#index"
-=======
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   root "users#index"
   resources :users, only: [:index, :new, :show, :create, :edit]
   resources :sessions, only: [:new, :create, :delete]
   get "/slots/:year/:month", to: "slots#month_index"
   get "/slots/:year/:month/:day", to: "slots#day_index"
   resources :slots, except: [:edit, :update, :index]
-  
+
   resources :feedbacks
->>>>>>> 1f2885b1eb8c88730e90f1bb82ebc1b32a098cf0
 end
