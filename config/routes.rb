f5f1b381ddaec156d3e179a0c25e05c2c898dfd8
Jviveiros <jviveiros@github.com>
2017-01-25 16:53:59 -0800

    Fixed routing issues

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,9 @@
 Rails.application.routes.draw do
   root "users#index"
-
-  resources :users, only: [:index, :new, :show, :create, :edit]
-
+  resources :users
   resources :sessions, only: [:new, :create, :destroy]
-
   get "/slots/:year/:month", to: "slots#month_index"
-
   get "/slots/:year/:month/:day", to: "slots#day_index"
-
   resources :slots, except: [:edit, :update, :index]
-
   resources :feedbacks
 end
