2236c81741545a083d4967bf322b0cbca3dde0f7
Meric BAL <MB@Meric-MacBook-Air.local>
2017-01-26 15:03:11 -0800

    Errors added to register page.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,21 +9,17 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find_by(id: params[:id])
+    # @user = User.find_by(id: params[:id])
+    @user = User.find(params[:id])
   end
 
-  def show
-    # @user = User.find_by(id: params[:id])
-    p '*' * 10
-    @user = User.find(params[:id])
-    p @user
-  end
   def create
     @user = User.new(user_params)
     if @user.save
       auto_login(@user)
       redirect_to user_path(@user)
     else
+      @errors = @user.errors.full_messages
       render action: "new"
     end
   end
@@ -32,9 +28,6 @@ class UsersController < ApplicationController
     @user = User.find_by(id: params[:id])
   end
 
-  def
-
-
   def destroy
     @user = User.find_by(id: params[:id])
     @user.destroy
@@ -42,7 +35,9 @@ class UsersController < ApplicationController
   end
 
   private
+
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :phase, :mentor)
   end
+
 end
