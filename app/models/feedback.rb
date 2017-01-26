20739d54e5bf0e8329b532e095e6373f8e683658
Meric BAL <MB@Meric-MacBook-Air.local>
2017-01-26 11:04:07 -0800

    Fixed an issue in feedback model.

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,6 +1,6 @@
 class Feedback < ApplicationRecord
 	belongs_to :user
-	belongs_to :slot
+	has_one :slot, through: :appointments
 
 	validates :rating, :appointment_id, :user_id, presence: true
 	validates :appointment_id, :user_id, uniqueness: true
