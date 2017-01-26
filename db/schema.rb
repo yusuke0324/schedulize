1d103f5ff7963374110c6949a1ed9aa01a8b5925
yusuke0324 <take.yusuke@gmail.com>
2017-01-26 10:38:01 -0800

    make seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170125193800) do
-
+ActiveRecord::Schema.define(version: 20170125195159) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,7 +23,6 @@ ActiveRecord::Schema.define(version: 20170125193800) do
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
   end
-
 
   create_table "slots", force: :cascade do |t|
     t.string   "title"
@@ -46,6 +44,6 @@ ActiveRecord::Schema.define(version: 20170125193800) do
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
-
   end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'date'
+Feedback.destroy_all
+User.destroy_all
+Slot.destroy_all
 
+now = Time.now
+tomorrow = now + 1.day
+yesterday = now - 1.day
+user = User.create(username: 'SeedMentor', phases: 1, mentor: true, email: 'mentor@seed.com', password: 'password')
 
-Feedback.create(rating: 1, comment: 'AAA', appointment_id: 1, user_id: 1)
-Feedback.create(rating: 5, comment: 'BBB', appointment_id: 2, user_id: 2	)
+5.times do |i|
+  Slot.create(title: "SeedSlot#{i}", capacity: i, start_time: now, end_time: tomorrow, user: user )
+end
+
+# Feedback.create(rating: 1, comment: 'AAA', appointment_id: 1, user_id: 1)
+# Feedback.create(rating: 5, comment: 'BBB', appointment_id: 2, user_id: 2	)
