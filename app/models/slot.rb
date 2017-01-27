4aac8e9bd1a447366c0290c07327062d3957408e
ejoreo <ejorear@gmail.com>
2017-01-27 15:14:57 -0800

    Add paginated list of reviews on user show

--- app/models/slot.rb
+++ app/models/slot.rb
@@ -1,4 +1,5 @@
 class Slot < ApplicationRecord
+  attr_accessor :skip_must_be_after_now_validation
   belongs_to :mentor, class_name: "User", foreign_key: "user_id"
   has_many :students, through: :appointments, source: :student
   has_many :appointments
@@ -12,15 +13,17 @@ class Slot < ApplicationRecord
   validate :start_must_be_after_now
   validates :title, presence: true
 
+  validate :start_must_be_after_now, unless: :skip_must_be_after_now_validation
+
 
 private
   def start_must_be_before_end_time
-      errors.add(:start_time, "start_time must be before end time") unless
+      errors.add(:start_time, "Start time must be before end of time") unless
           start_time < end_time
   end
 
   def start_must_be_after_now
-    errors.add(:start_time, "start_time can't be in the past") unless
-        start_time > DateTime.now
+    errors.add(:start_time, "Start time must be on the next calendar day") unless
+        start_time > DateTime.now.utc
   end
 end
